431f726dc809f11b6908409b6695bd2d01741821
daniclar156 <daniclar156@ed.amdsb.ca>
2017-05-29 13:40:14 -0400

    Add project files.

---
+++ hordeShooter/MainMenu.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace hordeShooter
+{
+    partial class MainMenu
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainMenu));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.playLabel = new System.Windows.Forms.Label();
+            this.leaderboardsLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Consolas", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(324, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(683, 112);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "HORDESHOOTER";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(230, 17);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(113, 80);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(1002, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(103, 94);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // playLabel
+            // 
+            this.playLabel.BackColor = System.Drawing.Color.Black;
+            this.playLabel.Font = new System.Drawing.Font("Consolas", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playLabel.ForeColor = System.Drawing.Color.White;
+            this.playLabel.Location = new System.Drawing.Point(434, 165);
+            this.playLabel.Name = "playLabel";
+            this.playLabel.Size = new System.Drawing.Size(442, 77);
+            this.playLabel.TabIndex = 3;
+            this.playLabel.Text = "Play";
+            this.playLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // leaderboardsLabel
+            // 
+            this.leaderboardsLabel.BackColor = System.Drawing.Color.Black;
+            this.leaderboardsLabel.Font = new System.Drawing.Font("Consolas", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.leaderboardsLabel.ForeColor = System.Drawing.Color.White;
+            this.leaderboardsLabel.Location = new System.Drawing.Point(427, 386);
+            this.leaderboardsLabel.Name = "leaderboardsLabel";
+            this.leaderboardsLabel.Size = new System.Drawing.Size(453, 78);
+            this.leaderboardsLabel.TabIndex = 4;
+            this.leaderboardsLabel.Text = "Leaderboards";
+            this.leaderboardsLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Controls.Add(this.leaderboardsLabel);
+            this.Controls.Add(this.playLabel);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "MainMenu";
+            this.Size = new System.Drawing.Size(1366, 768);
+            this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.MainMenu_PreviewKeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label playLabel;
+        private System.Windows.Forms.Label leaderboardsLabel;
+    }
+}
---
+++ hordeShooter/MainMenu.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hordeShooter
+{
+    public partial class MainMenu : UserControl
+    {
+        //index for menu items
+        int index = 0;
+        int lastIndex = 0;
+
+        public MainMenu()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+        }
+
+        private void MainMenu_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            lastIndex = index;
+            Form form = this.FindForm();
+            //to make sure that if you click to the bottom or top you will go to the other end instead of just stopping
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    if (index != 0)
+                        index--;
+                    else
+                    {
+                        index = 1;
+                    }
+                    break;
+                case Keys.Down:
+                    if (index != 1)
+                        index++;
+                    else
+                    {
+                        index = 0;
+                    }
+                    break;
+            }
+
+            switch (lastIndex)
+            {
+                case 0:
+                    playLabel.BackColor = Color.Black;
+                    break;
+                case 1:
+                    leaderboardsLabel.BackColor = Color.Black;
+                    break;
+            }
+
+            switch (index)
+            {
+                case 0:
+                    playLabel.BackColor = Color.Red;
+                    break;
+                case 1:
+                    leaderboardsLabel.BackColor = Color.Red;
+                    break;
+            }
+        }
+    }
+}
